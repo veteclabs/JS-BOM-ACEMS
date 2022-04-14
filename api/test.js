df6d1469d05a14ef78c4cbf0137cc22f8e0057cd
markcha <21thpoem@naver.com>
2022-04-14 23:42:03 +0900

    1. 시간 요일, 주차 비교 이후 압력 이상일 경우 OFF 전위 2. STAY 상태일 경우 해당 루프 바로 종료 3. group function 완료 4. 제어 함수에서 제어될 상태와 해당 설비의 상태가 같으면 false 다르면 true를 통해 제어가 이루어졌음을 리턴

--- api/test.js
+++ api/test.js
@@ -1,9 +1,7 @@
 const schedule = require("node-schedule");
 const axios = require("axios");
-
-
-
-
+const pool = require('../config/database');
+const Dayjs = require("dayjs");
 
 const rule = new schedule.RecurrenceRule();
 rule.second = 30;
@@ -20,9 +18,17 @@ if(contain === -1) {
     console.log(-1)
 }
 
-for (let scheduleOne in scheduleList) {
-    console.log(scheduleOne)
+
+
+exports.a = async () => {
+    try {
+
+        console.log(week[0][0].WEEK)
+    } catch(e) {
+
+    }
 }
+this.a()
 exports.threadManeger = async () => {
     console.log("managing thread.")
     try {
@@ -39,46 +45,111 @@ exports.threadManeger = async () => {
 }
 exports.checkPoserState = async (scheduleId) => {
     try {
-        let schedules = await axios.get(`http://localhost:8031/api/schedule/${scheduleId}`);
-        let powerState = false;
+        const now = Dayjs();
+        const conn = await pool.getConnection();
+
+        let week = await conn.query('CALL get_week_number(?)', now.toDate());
+        week = week[0][0].WEEK;
+        let dayOfWeek = now.day()
+        let schedules = await axios.get(`http://112.216.32.6:8031/api/schedule/${scheduleId}`);
+        let powerState = '';
         for (let schedule of schedules.data) {
-            if (schedule.isGroup) {
-                await this.groupOrder(schedule.scheduleId);
-            } else {
-                await this.controllFacility(schedule.groupId);
+            console.log(schedule.isGroup)
+            if (schedule.isActive) {
+                let startTime = new Date()
+                let now2 = new Date();
+                let endTime = new Date()
+                startTime.setHours(Number(schedule.startTime.substring(0, 2)), Number(schedule.startTime.substring(3, 5)), Number(schedule.startTime.substring(6, 8)))
+                endTime.setHours(Number(schedule.stopTime.substring(0, 2)), Number(schedule.stopTime.substring(3, 5)), Number(schedule.stopTime.substring(6, 8)))
+                if ((startTime <= now2 && now2 <= endTime) && (dayOfWeek in schedule.dayOfWeeks) && (week in schedule.weeks)) {
+                    if (schedule.min > schedule.pressure) {
+                        powerState = 'ON'
+                    } else if (schedule.max < schedule.pressure) {
+                        powerState = 'OFF'
+                    } else {
+                        powerState = 'STAY'
+                    }
+                } else {
+                    powerState = 'OFF'
+                }
+                if (powerState !== 'STAY') {
+                    console.log(week)
+
+                    if (schedule.isGroup) {
+                        await this.groupOrder(scheduleId, week, powerState);
+                    } else {
+                        // await this.controllFacility(schedule.groupId);
+                    }
+                }
             }
         }
     } catch (e) {
         console.log(e)
     }
 }
-exports.groupOrder = async (scheduleId) => {
+this.checkPoserState(1);
+exports.groupOrder = async (scheduleId, week, powerState) => {
+    console.log(scheduleId)
     try {
-        let weekId = 1;
-        let orders = await axios.get(`http://localhost:8031/api/schedule/${scheduleId}/week/${weekId}`);
+        let orders = await axios.get(`http://112.216.32.6:8031/api/schedule/${scheduleId}/week/${week}`);
+
         orders = orders.data
-        let powerState = false;
-        orders = orders.sort((a,b)=> a.order < b.order);
-        for(let order of orders) {
-            await this.controllFacility(order.groupId);
+        // orders = orders.sort((a,b)=> a.order < b.order);
+        let controllerResult = null;
+        if (!orders.length !== 0) {
+            for (let order of orders) {
+                controllerResult = await this.controllFacility(order.groupId, powerState);
+                if (controllerResult) {
+                    return true;
+                }
+            }
         }
-        return orders
+        return true;
     } catch (e) {
         console.log(e)
     }
 }
-exports.controllFacility = async (groupId) => {
+exports.controllFacility = async (groupId, powerState) => {
     try {
         const params = {
             tagType: "POWER",
             equipmentType: "AIR_COMPRESSOR"
         }
-        let tags = await axios.get(`http://localhost:8031/api/tags/${groupId}`, {params:params});
-        tags = tags.data
-        console.log(tags)
+        // let tags = await axios.get(`http://localhost:8031/api/tags/${groupId}`, {params:params});
+        // tags = tags.data
+        return true;
     } catch (e) {
         console.log(e)
     }
 }
 // this.threadManeger();
 
+
+
+Date.prototype.getWeek = function (dowOffset) {
+    /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+
+    dowOffset = typeof(dowOffset) == 'number' ? dowOffset : 0; // dowOffset이 숫자면 넣고 아니면 0
+    var newYear = new Date(this.getFullYear(),0,1);
+    var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+    day = (day >= 0 ? day : day + 7);
+    var daynum = Math.floor((this.getTime() - newYear.getTime() -
+        (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+    var weeknum;
+    //if the year starts before the middle of a week
+    if(day < 4) {
+        weeknum = Math.floor((daynum+day-1)/7) + 1;
+        if(weeknum > 52) {
+            let nYear = new Date(this.getFullYear() + 1,0,1);
+            let nday = nYear.getDay() - dowOffset;
+            nday = nday >= 0 ? nday : nday + 7;
+            /*if the next year starts before the middle of
+              the week, it is week #1 of that year*/
+            weeknum = nday < 4 ? 1 : 53;
+        }
+    }
+    else {
+        weeknum = Math.floor((daynum+day-1)/7);
+    }
+    return weeknum;
+};
