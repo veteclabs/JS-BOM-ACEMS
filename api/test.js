554504abeb533423c381f9149c6980f07e7dc048
markcha <21thpoem@naver.com>
2022-04-14 16:19:10 +0900

    [frontend] axios test

--- api/test.js
+++ api/test.js
@@ -1,5 +1,5 @@
 const schedule = require("node-schedule");
-
+const axios = require("axios");
 const rule = new schedule.RecurrenceRule();
 rule.second = 30;
 schedule.scheduleJob("12",rule, function(){
@@ -7,3 +7,8 @@ schedule.scheduleJob("12",rule, function(){
 });
 
 console.log(schedule.scheduledJobs)
+
+
+exports.threadManeger() {
+    axios.get("http://112.216.32.6:8031/api/schedules")
+}
